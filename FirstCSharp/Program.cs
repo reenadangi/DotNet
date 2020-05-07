f46e0a920829233ed1693fd42e347026c4f393d9
reenadangi <reena.dangi@gmail.com>
2020-05-07 10:57:37 -0500

    Csharp multidim array

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -22,13 +22,30 @@ namespace FirstCSharp
             // Console.WriteLine(evenOrOdd(23));
             printArray();
             printList();
+            printDict();
+        }
+
+        static void printDict(){
+            Dictionary<string,string> profile=new Dictionary<string, string>();
+            profile.Add("Name","Reena");
+            profile.Add("Language","Python");
+            profile.Add("location","Chicago");
+            Console.WriteLine(profile["location"]);
+            foreach(KeyValuePair<string,string> entry in profile){
+                Console.WriteLine($"{entry.Key}-{entry.Value}" );
+            }
+
         }
         static void printList(){
             List<string> dojos=new List<string>();
             dojos.Add("Reena");
-
-
-            
+            dojos.Add("Lee");
+            dojos.Add("cass");
+            dojos.Insert(2,"mike");
+            dojos.Remove("Reena");
+            foreach(string dojo in dojos){
+                Console.WriteLine(dojo);
+            }
 
         }
         static void printArray(){
@@ -38,8 +55,12 @@ namespace FirstCSharp
             // For Arrays initialized this way, the length is determined by the size of the given data
             int[] numArray2 = {1,2,3,4,6};
             int[] nums={1,2,34,22};
-            int[] array3;
-            array3 = new int[] {1,3,5,7,9};
+            
+            int[,] arr2D=new int[3,3]{{2,4,6},{3,6,9},{4,8,12}};
+            Console.WriteLine("2d array"+ arr2D[0,1]);
+
+
+            int[] array3 = new int[] {1,3,5,7,9};
             for(int i=0;i<nums.Length;i++){
                Console.WriteLine(nums[i]);
             }
@@ -47,6 +68,25 @@ namespace FirstCSharp
             foreach(string dojo in dojos) {
                 Console.WriteLine(dojo);
             }
+            int[,] multi=new int[10,10];
+            for(int i=1;i<=10;i++){
+                for(int j=1;j<=10;j++){
+                    multi[i-1,j-1]=i*j;
+                }
+            }
+            Console.WriteLine(multi);
+            for(int i=0;i<10;i++){
+                 string str="[";
+                 for(int j=0;j<10;j++){
+                    str= str+ multi[i,j].ToString()+",";
+                 }
+                  str= str+"]";
+                  Console.WriteLine(str);
+            }
+            foreach(int num in multi){
+                Console.WriteLine("This is my table" +num);
+            }
+
 
         }
         static string evenOrOdd(int num){
