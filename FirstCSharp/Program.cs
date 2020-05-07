d30cb09983779edc8bd3a3c8e52584c87f109dee
reenadangi <reena.dangi@gmail.com>
2020-05-07 08:02:20 -0500

    Csharp

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+            int i=0;
+            while(i<10){
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
