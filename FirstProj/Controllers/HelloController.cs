791cfbc4f9cb4a1cc89f14ebc741cefad5f09a50
reenadangi <reena.dangi@gmail.com>
2020-05-10 14:52:29 -0500

    Asp MVC Razer

--- FirstProj/Controllers/HelloController.cs
+++ FirstProj/Controllers/HelloController.cs
@@ -1,15 +1,48 @@
+using Hello.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic; 
+using System;
 public class HelloController:Controller{
 [Route("")]
-public ViewResult Index(){
+[HttpGet]
+public IActionResult Index(){
     // ViewResult myView=View();
     // return "Hello Bello";
     return View();
 }
-[Route("profile{name}")]
+
+[HttpPost("profile")]
+// Mostly after form submition
+public IActionResult Profile(string Login,string Password,string track,string isDojo){
+    Console.WriteLine(Login+Password+track+isDojo);
+    Ninja Ninja=new Ninja(){
+        Name=Login,
+        Password=Password
+    };
+    ViewBag.Ninja=Ninja;
+    
+    return View("Profile");
+}
+[Route("profile/{name}")]
+[HttpGet]
 // Mostly after form submition
 public IActionResult Profile(string name){
-    return RedirectToAction("Profile",new {name=name});
+    Console.WriteLine("hello");
+    string[] languages;
+    // if(name.ToLower()=="rina"){
+    //     languages=new string[]{"NodeJS","java","c#","python"};
+    // }
+    // else{
+    //     languages=new string[]{"python","Java"};
+    // }
+    // ViewBag.Languages=languages;
+    // ViewBag.Name=name;
+    ViewBag.Ninja=new Ninja();
+    
+    
+    return View("Profile");
+
+    // return RedirectToAction("Profile");
 }
 
 
---
+++ FirstProj/Models/Ninja.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Hello.Models{
+public class Ninja{
+    public string Name{get;set;}
+    // public string[] languages{get;set;}
+    public string Password{get;set;}
+    public Ninja(){
+        // Name="Dev";
+        // languages=new string[]{"NodeJS","java","c#","python"};
+        
+    }
+
+
+}
+}
